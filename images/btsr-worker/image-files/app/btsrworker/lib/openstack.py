f6d195e3f7353827f69ddc11c3faf948f84c424e
kylep <kyle.pericak@breqwatr.com>
2021-05-20 00:23:08 +0000

    calls to openstack working

--- images/btsr-worker/image-files/app/btsrworker/lib/openstack.py
+++ images/btsr-worker/image-files/app/btsrworker/lib/openstack.py
@@ -3,6 +3,9 @@ import requests
 from btsrworker.lib.common import env
 
 
+class OpenstackException(Exception):
+    """ Something has gone wrong with a call to OpenStack """
+
 def get_os_env():
     """ Return the OpenStack environment values in a dictionary """
     keys = ["OS_PROJECT_DOMAIN_NAME", "OS_USER_DOMAIN_NAME", "OS_PROJECT_NAME", "OS_TENANT_NAME", "OS_USERNAME", "OS_PASSWORD", "OS_AUTH_URL", "OS_INTERFACE", "OS_ENDPOINT_TYPE", "OS_IDENTITY_API_VERSION", "OS_REGION_NAME", "OS_AUTH_PLUGIN"]
@@ -11,11 +14,13 @@ def get_os_env():
       os_env[key] = env(key)
     return os_env
 
+
 def get_token(os_env):
     """ Get an OpenStack token and API catalog using global vars """
     # Build json data to send to openstack api
     os_username = os_env["OS_USERNAME"]
     os_password = os_env["OS_PASSWORD"]
+    os_auth_url = os_env["OS_AUTH_URL"]
     os_user_domain_name = os_env["OS_USER_DOMAIN_NAME"]
     os_project_name = os_env["OS_PROJECT_NAME"]
     os_project_domain_name = os_env["OS_PROJECT_DOMAIN_NAME"]
@@ -42,6 +47,30 @@ def get_token(os_env):
     token_url = f"{os_auth_url}/auth/tokens"
     resp = requests.post(token_url, json=login_data, verify=False)
     if resp.status_code != 201:
-        error("Openstack login failed")
+        raise OpenstackException(f"{resp.status_code}: {resp.reason}")
     token = resp.headers["X-Subject-Token"]
     return token, resp.json()
+
+
+def os_headers(token):
+    """ Return the format expected by openstack/trilio for HTPP headers """
+    return {"Content-Type": "application/json", "X-Auth-Token": token}
+
+
+def os_endpoint(name, token_data):
+    """ Return the OpenStack endpoint from the token's JSON data """
+    os_interface = get_os_env()["OS_INTERFACE"]
+    catalog = next(c for c in token_data["token"]["catalog"] if c["name"] == name)
+    return next(e["url"] for e in catalog["endpoints"] if e["interface"] == os_interface)
+
+
+def get_servers(token, token_data):
+    """ Return a list of servers in the scoped project """
+    nova_url = os_endpoint("nova", token_data)
+    servers_url = f"{nova_url}/servers"
+    headers = os_headers(token)
+    resp = requests.get(servers_url, headers=headers)
+    if resp.status_code != 200:
+        raise OpenstackException(f"{resp.status_code}: {resp.reason}")
+    return resp.json()["servers"]
+
--- images/btsr-worker/image-files/app/btsrworker/main.py
+++ images/btsr-worker/image-files/app/btsrworker/main.py
@@ -4,3 +4,7 @@ import btsrworker.lib.openstack as os
 def main():
     env = os.get_os_env()
     print("BTSR WORKER")
+    token, token_data = os.get_token(env)
+    servers = os.get_servers(token, token_data)
+    from pprint import pprint
+    pprint(servers)
